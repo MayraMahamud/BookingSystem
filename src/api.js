b0c320b4896cadb81fc614fa7e8f513ff360bf5d
MMaha1 <mayraam049@gmail.com>
2024-11-26 09:32:35 +0100

    -

--- src/api.js
+++ src/api.js
@@ -4,28 +4,3 @@ export const fetchRooms = async () => {
 };
 
 
-/*export const AddBooking = async (booking) => {
-    console.log(booking)
-    booking.Id=1
-   
-   
-    try{
-    const response = await fetch(`${"https://boolean-uk-api-server.fly.dev/MayraMahamud/post"}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',}, 
-            body: JSON.stringify(post),
-        });
-        if(!response.ok){
-            throw new Error('Failed creating post');
-        }
-        const data =await response.json();
-        return data;
-    }
-    catch (error) {
-        console.error('error', error);
-        throw error;
-    }
-       
-    };
-*/
